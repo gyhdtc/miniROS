f6e0a76a36992b3832f27a40e0933d5ee946270d
“gyhdtc” <gyh108wy@163.com>
2021-03-12 22:46:12 +0800

    1

--- test/shittest.cpp
+++ test/shittest.cpp
@@ -1,54 +1,21 @@
 #include <iostream>
-#include <thread>
-#include <ctype.h>
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-class shittest
-{
-public:
-    /* data */
-    int* x;
-    string name;
-public:
-    shittest(string);
-    shittest(const shittest& gyh)
-    {
-        x = gyh.x;
-        name = gyh.name;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> num;
+    for (int i = 0; i < n; ++i) {
+        int t;
+        cin >> t;
+        num.push_back(t);
     }
-    ~shittest();
-};
-
-shittest::shittest(string s)
-{
-    name = s;
-    cout << name << " - s" << endl;
-    x = new int(2);
-}
-
-shittest::~shittest()
-{
-    cout << name << " - c" << endl;
-    cout << "ccc" << endl;
-    delete x;
-}
-
-void func(shittest h)
-{
-    cout << 3 << endl;
-    // shittest m = h;
-    cout << 4 << endl;
-    cout << "func " << h.x << endl;
-    // delete m;
-    // delete h;
-    cout << 5 << endl;
-    return;
-}
-
-int main()
-{
-    uint8_t t[4] = {1,2,3,4};
-    uint32_t a = uint8_t(t);
-    cout << a << endl;
-    return 0;
+    if (n < 3) {
+        cout << -1 << endl;
+        return 0;
+    }
+    
 }
