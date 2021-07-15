bcbd169e37ff76415ac15165ad8f2a9c2993d493
gyhdtc <gyh108wy@163.com>
2021-07-15 16:24:08 +0800

    1

--- testv2.0/src/test.cpp
+++ testv2.0/src/test.cpp
@@ -1,30 +1,55 @@
 #include "../include/headv2.0.h"
 
+// class A {
+// private:
+//     int shit;
+// public:
+//     A() {
+//         cout << "1\n";
+//     }
+//     A(A& a) {
+//         cout << "2\n";
+//     }
+// };
+// void th(shared_ptr<A>& a) {
+//     cout << a.use_count() << endl;
+//     sleep(2);
+//     cout << a.use_count() << endl;
+// }
+// void func() {
+//     shared_ptr<A> shit(new A);
+//     // thread t1(th, shit);
+//     // t1.detach();
+//     th(shit);
+//     sleep(1);
+// }
+// int main() {
+//     func();
+//     sleep(5);
+//     return 0;
+// }
 class A {
 private:
     int shit;
 public:
-    A() {
-        cout << "1\n";
-    }
-    A(A& a) {
-        cout << "2\n";
+    virtual void func() {
+        printf("gyh\n");
     }
 };
-void th(shared_ptr<A>& a) {
-    cout << a.use_count() << endl;
-    sleep(2);
-    cout << a.use_count() << endl;
-}
-void func() {
-    shared_ptr<A> shit(new A);
-    // thread t1(th, shit);
-    // t1.detach();
-    th(shit);
-    sleep(1);
-}
+class B : public A {
+private:
+    int a;
+public:
+    virtual void func() {
+        printf("gyh2\n");
+    }
+};
+
+
 int main() {
-    func();
-    sleep(5);
+    A* a = new B;
+    a->func();
+    B* b = (B*)a;
+    b->func();
     return 0;
 }
